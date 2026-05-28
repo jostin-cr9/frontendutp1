e0d72f80d8574f78f06b14eed6f6cdb54419c4b8
jostin-cr9 <jostinaparicio440@gmail.com>
2026-05-28 13:44:27 -0500

    -

--- src/app/pages/inicio/inicio.ts
+++ src/app/pages/inicio/inicio.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css'
 })
